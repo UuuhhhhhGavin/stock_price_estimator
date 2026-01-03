159540513728ccdb1c5b22d9637484e2af0c5ff3
UuuhhhhhGavin <116385663+UuuhhhhhGavin@users.noreply.github.com>
2026-01-02 23:19:16 -0600

    Update pytest.py

--- pytest.py
+++ pytest.py
@@ -1,5 +1,11 @@
-
+import os
 
 test_var="hello, this is python"
 print(test_var)
 
+project_dir = os.getcwd()
+file_path=os.path.join(project_dir, 'test_output.txt'
+                       )
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(test_var)
+print(f"File '{file_path}' saved successfully.")
